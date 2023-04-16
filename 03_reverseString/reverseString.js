2e2dffe95cf63674dd67d3a587b75adcf9af4d33
Michael Doocey <michael.michaeldoocey.doocey@gmail.com>
2023-04-16 09:32:37 +0200

    solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    let chars = string.split("");
+    chars = chars.reverse();
+    for (const char of chars) {
+        output += char;
+    }
+    return output;
 };
 
 // Do not edit below this line
