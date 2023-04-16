7137c88978fb7600f8882f589b794e094591a418
Michael Doocey <michael.michaeldoocey.doocey@gmail.com>
2023-04-16 15:00:29 +0200

    solve tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let result = (num - 32) * 5 / 9;
+  // return parseFloat(result.toFixed(1)); // toFixed sets decimal places; returns string; hence parseFloat
+  return Math.round(result * 10) / 10; // alternative to toFixed; returns whole numbers without decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+ let result = (num *  9 / 5 ) + 32;
+//  return parseFloat(result.toFixed(1));
+ return Math.round(result * 10) / 10;  
 };
 
 // Do not edit below this line
