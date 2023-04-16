9a0e017c024a1b2be94bc88bf75d13b19be973dc
Michael Doocey <michael.michaeldoocey.doocey@gmail.com>
2023-04-16 13:57:25 +0200

    solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstIn, secondIn) {
+    let smallIn;
+    let bigIn;
+    
+    if (firstIn > secondIn) {
+        smallIn = secondIn;
+        bigIn = firstIn;
+    } else {
+        smallIn = firstIn;
+        bigIn = secondIn;
+    } // so the larger number can be last
+    
+    if (smallIn + bigIn < bigIn || typeof (smallIn + bigIn)  !== 'number') {
+        return 'ERROR';
+    } // tests for negative start; requires that start < end
 
+    let sum = 0;
+    
+    for (let i = smallIn; i <= bigIn; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
