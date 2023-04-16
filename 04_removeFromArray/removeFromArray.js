5048fb4830422eeb6137612420d0a9c5d8f69f9e
Michael Doocey <michael.michaeldoocey.doocey@gmail.com>
2023-04-16 11:09:12 +0200

    pass removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
 const removeFromArray = function() {
-
+    let myArray = arguments[0]; // returns  array
+    for (let i = 1; i < arguments.length; i++) {
+        let searchFor = arguments[i];
+        for (let j = 0; j < myArray.length; j++) {
+            if (myArray[j] === searchFor) {
+                myArray.splice(j, 1);
+            }
+        }
+    }
+    return myArray;
 };
+
+// for every argument after the first one
+// loop through the array given in the first argument
+// if the array item matches the argument being tested
+// remove that item from the array 
 
 // Do not edit below this line
 module.exports = removeFromArray;
